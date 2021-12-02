0c1b24d9a85d4204af2bb6986bd560a861673d19
nizarlalji <95396289+nizarlalji@users.noreply.github.com>
2021-12-03 10:28:33 +1100

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -6,3 +6,9 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "example22"
+  location = "eastus"
+}
